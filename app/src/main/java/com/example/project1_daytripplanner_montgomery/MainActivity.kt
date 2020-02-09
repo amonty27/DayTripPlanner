43188ac207c65b286ceb32e66f6cb5832244de41
Alicia <amonty27@gwu.edu>
2020-02-08 23:04:54 -0500

    Added SeekBar for Activities

--- app/src/main/java/com/example/project1_daytripplanner_montgomery/MainActivity.kt
+++ app/src/main/java/com/example/project1_daytripplanner_montgomery/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.project1_daytripplanner_montgomery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AbsSeekBar
 import android.widget.SeekBar
 import android.widget.TextView
 
@@ -10,6 +9,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var foodNumber: TextView
     lateinit var foodSeekBar: SeekBar
+
+    lateinit var activitySeekBar: SeekBar
+    lateinit var activityNumber: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +21,26 @@ class MainActivity : AppCompatActivity() {
         foodSeekBar = findViewById(R.id.foodSeekBar) as SeekBar
         foodNumber = findViewById(R.id.foodNumber) as TextView
 
-        foodSeekBar.max = 10
+        activitySeekBar = findViewById(R.id.activitySeekBar) as SeekBar
+        activityNumber = findViewById(R.id.activityNumber) as TextView
+
+
         foodSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 foodNumber.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+
+        activitySeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                activityNumber.text = progress.toString()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
