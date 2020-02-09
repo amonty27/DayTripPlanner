d8b3b82fcb9b29f96009ce4930b6d230b642812f
Alicia <amonty27@gwu.edu>
2020-02-08 22:46:26 -0500

    Added SeekBar and Spinners
    - added SeekBar for the food options with the value between 0 and 10
    - added text to side of seek bar that records progress of the bar
    - added food and activies spinner attributes with 5 selections each
    - changed names of variables to be more precise for their function

--- app/src/main/java/com/example/project1_daytripplanner_montgomery/MainActivity.kt
+++ app/src/main/java/com/example/project1_daytripplanner_montgomery/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.project1_daytripplanner_montgomery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AbsSeekBar
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var foodNumber: TextView
+    lateinit var foodSeekBar: SeekBar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        foodSeekBar = findViewById(R.id.foodSeekBar) as SeekBar
+        foodNumber = findViewById(R.id.foodNumber) as TextView
+
+        foodSeekBar.max = 10
+        foodSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                foodNumber.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+
+
     }
 }
