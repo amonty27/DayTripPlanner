c8eed4f376e0e9a91f95515a4ee9288059befc7e
Alicia <amonty27@gwu.edu>
2020-02-11 16:54:32 -0500

    Implemented SharedPreferences and Dialog box
    - Implemented SharedPreferences for the userInput of the address
    - Implemented a Dialog box that returns the userInput of the address and uses a radio button for the search result

--- app/src/main/java/com/example/project1_daytripplanner_montgomery/MainActivity.kt
+++ app/src/main/java/com/example/project1_daytripplanner_montgomery/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.project1_daytripplanner_montgomery
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.SeekBar
-import android.widget.TextView
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,21 +19,54 @@ class MainActivity : AppCompatActivity() {
     lateinit var activitySeekBar: SeekBar
     lateinit var activityNumber: TextView
 
+    lateinit var searchButton : Button
+
+    lateinit var userInput: EditText
+
+    lateinit var foodSpinner: Spinner
+    lateinit var activitySpinner: Spinner
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        foodSeekBar = findViewById(R.id.foodSeekBar) as SeekBar
-        foodNumber = findViewById(R.id.foodNumber) as TextView
+        //initializing variables
+        foodSeekBar = findViewById(R.id.foodSeekBar)
+        foodNumber = findViewById(R.id.foodNumber)
 
-        activitySeekBar = findViewById(R.id.activitySeekBar) as SeekBar
-        activityNumber = findViewById(R.id.activityNumber) as TextView
+        activitySeekBar = findViewById(R.id.activitySeekBar)
+        activityNumber = findViewById(R.id.activityNumber)
+
+        searchButton = findViewById(R.id.searchButton)
+
+        userInput = findViewById(R.id.address)
+
+        foodSpinner = findViewById(R.id.foodSpin)
+        activitySpinner = findViewById(R.id.activitySpin)
+
+
+        // set the search button to false so that the user cannot search until conditions cleared
+        //searchButton.isEnabled = false
+        //set up the saved preferences to save the Address
+        val preferences = getSharedPreferences("dayTripPlaner_data", Context.MODE_PRIVATE)
+
+        //foodSpinner = preferences.getString("FOOD_SPIN","")
+
+
+
+
+
+
+
 
 
         foodSeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 foodNumber.text = progress.toString()
+                //preferences.edit().putString("FOOD_SPIN", foodSpinner.toString().get(1).toString())
+                    //userInput.text
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -37,6 +76,14 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+
+
+
+
+
+
+
 
         activitySeekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -50,6 +97,43 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+
+
+
+
+
+
+
+
+        userInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                preferences.edit().putString("SEARCH",userInput.text.toString()).apply()
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+        })
+
+        searchButton.setOnClickListener{
+            val array = listOf(preferences.getString("SEARCH",""))
+            val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice)
+            arrayAdapter.addAll(array)
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("dialog Box")
+                .setAdapter(arrayAdapter) { dialog, which ->
+                    Toast.makeText(this, "You picked: ${preferences.getString("SEARCH","")}", Toast.LENGTH_SHORT).show()
+                }
+
+
+
+            ///builder.setSingleChoiceItems(array,-1)
+            builder.show()
+        }
+
 
 
     }
